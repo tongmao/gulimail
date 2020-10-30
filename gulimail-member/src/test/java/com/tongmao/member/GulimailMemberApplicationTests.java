71ecb31b048a67136838ef5eaf4ec26712ed1276
Tongmao <1026396280@qq.com>
2020-10-30 16:41:47 +0800

    项目初始结构创建

---
+++ gulimail-member/src/test/java/com/tongmao/member/GulimailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongmao.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
